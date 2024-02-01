e07a07ca522fde431b542e623ef5069c7cd67227
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-02-01 10:41:33 -0500

    finished post route in category file. Fixed some things in module folder

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,30 +4,28 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-try {
-  
-  
-  const categories = await Category.findAll({
-    include: Product
-  });
-  console.log(categories)
-  if (!categories) {
-    return res.status(404).json({
-      message: 'Cannot get categories'
-    })
-    // return
+  try {
+
+
+    const categories = await Category.findAll({
+      include: Product
+    });
+    console.log(categories)
+    if (!categories) {
+      return res.status(404).json({
+        message: 'Cannot get categories'
+      })
+      // return
+    }
+
+    res.status(200).json(categories)
+
+  } catch (error) {
+    res.status(500).send('interal error')
   }
-  
-  res.status(200).json(categories)
-  
-} catch (error) {
-  res.status(500).send('interal error')
-}
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const category = await Category.findByPk(req.params.id, {
       include: Product,
@@ -44,13 +42,22 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+
+router.put('/:id', async (req, res) => {
+ 
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
